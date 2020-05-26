bugfix metric calculation ner with bert

input_fn/input_fn_nlp/input_fn_ner.py
@@ -32,17 +32,17 @@ class InputFnNER(InputFnNLPBase):
 
     def get_shapes_types_defaults(self):
 
-        input_shapes = {'sentence': [None], 'sentencelength': [None]}
+        input_shapes = {'sentence': [None]}
 
-        tgt_shapes = {'tgt': [None]}
+        tgt_shapes = {'tgt': [None],'targetmask':[None]}
 
-        input_types = {'sentence': tf.int32, 'sentencelength': tf.int32}
+        input_types = {'sentence': tf.int32}
 
-        tgt_types = {'tgt': tf.int32}
+        tgt_types = {'tgt': tf.int32,'targetmask':tf.int32}
 
-        input_defaults = {'sentence': 0, 'sentencelength': 0}
+        input_defaults = {'sentence': 0}
 
-        tgt_defaults = {'tgt': 0}
+        tgt_defaults = {'tgt': 0,'targetmask':0}
 
         self._shapes = input_shapes, tgt_shapes
         self._types = input_types, tgt_types
@@ -84,14 +84,14 @@ class InputFnNER(InputFnNLPBase):
                 last_index=None
         #Add SOS-Tag and EOS-Tag
         enc_sentence=[self._tok_vocab_size]+enc_sentence+[self._tok_vocab_size+1]
+        targetmask=[0]+len(tar_real)*[1] + [0]
         tar_real=[self.get_num_tags()]+tar_real+[self.get_num_tags()+1]
-        sentencelength=[len(tar_real)]
         if self._flags.predict_mode:
-            inputs = {'sentence':[enc_sentence],'sentencelength':[sentencelength]}
-            tgts = {'tgt': [tar_real]}
+            inputs = {'sentence':[enc_sentence]}
+            tgts = {'tgt': [tar_real],'targetmask':targetmask}
             return inputs, tgts, training_data
-        inputs = {'sentence':enc_sentence,'sentencelength':sentencelength}
-        tgts = {'tgt': tar_real}
+        inputs = {'sentence':enc_sentence}
+        tgts = {'tgt': tar_real,'targetmask':targetmask}
 
         return inputs, tgts
 
@@ -109,5 +109,6 @@ class InputFnNER(InputFnNLPBase):
             #print("Yield Sentence")
             for i in range(len(training_data)):
                 yield self._parse_fn(training_data[i])
+
 
 

model_fn/model_fn_nlp/model_fn_ner.py
@@ -37,13 +37,11 @@ class ModelNER(ModelBase):
     def get_predictions(self):
         return {
             "classes": self._graph_out['pred_ids'],
-            "probabilities": self._graph_out['probabilities'],
-            "sentencelength": self._graph_out['sentencelength']
+            "probabilities": self._graph_out['probabilities']
         }
 
     def get_placeholder(self):
-        ret = {"sentence": tf.compat.v1.placeholder(tf.int32, [None, None, None], name="inputSentence"),
-                "sentencelength": tf.compat.v1.placeholder(tf.int32, [None], name="inputSentencelength"),
+        ret = {"sentence": tf.compat.v1.placeholder(tf.int32, [None, None, None], name="inputSentence")
                 }
         for idx, _ in enumerate(self.flags.add_types):
             ret['feat{}'.format(idx)] = tf.compat.v1.placeholder(tf.int32, [None, None], name='feature{}'.format(idx))
@@ -57,48 +55,27 @@ class ModelNER(ModelBase):
 
     def get_loss(self):
         tags = self._targets['tgt']
-        sentencelength=self._graph_out['sentencelength']
-        weights = tf.sequence_mask(sentencelength)
+        weights=tf.cast(self._targets['targetmask'],tf.bool)
         return tf.compat.v1.losses.sparse_softmax_cross_entropy(labels=tags, logits=self._graph_out['logits'],weights=weights,reduction=tf.compat.v1.losses.Reduction.MEAN)
 
     def loss(self, predictions, targets):
         tags = targets['tgt']
-        sentencelength=predictions['sentencelength']
-        weights = tf.sequence_mask(sentencelength)
+        weights=tf.cast(targets['targetmask'],tf.bool)
         rvalue=tf.compat.v1.losses.sparse_softmax_cross_entropy(labels=tags, logits=predictions['logits'],weights=weights)#, reduction=tf.compat.v1.losses.Reduction.MEAN)
         return rvalue
 
     def get_call_graph_signature(self):
-        call_graph_signature = [{'sentence':tf.TensorSpec(shape=[None,None], dtype=tf.int32),
-                                 'sentencelength':tf.TensorSpec(shape=[None, None], dtype=tf.int32)},
-                                {'tgt': tf.TensorSpec(shape=[None, None], dtype=tf.int32)}]
+        call_graph_signature = [{'sentence':tf.TensorSpec(shape=[None,None], dtype=tf.int32)},
+                                {'tgt': tf.TensorSpec(shape=[None, None], dtype=tf.int32),
+                                 'targetmask':tf.TensorSpec(shape=[None, None], dtype=tf.int32)}]
         return call_graph_signature
 
     def to_tensorboard(self, graph_out_dict, targets, input_features):
+        weights=tf.cast(targets['targetmask'],tf.bool)
         self.metrics[self._mode]["loss"](graph_out_dict["loss"])
-        self.metrics[self._mode]["accuracy"].update_state(targets['tgt'], graph_out_dict['pred_ids'], tf.sequence_mask(graph_out_dict['sentencelength']))
+        self.metrics[self._mode]["accuracy"].update_state(targets['tgt'], graph_out_dict['pred_ids'], weights)
         true_positiv_indexes=tf.cast(tf.raw_ops.Equal(x=targets['tgt'],y=graph_out_dict['pred_ids']),tf.int32)
-        self.metrics[self._mode]["precision"].update_state(true_positiv_indexes, tf.cast(tf.raw_ops.NotEqual(x=graph_out_dict['pred_ids'],y=self._tag_string_mapper.get_oov_id()),tf.int32), tf.sequence_mask(graph_out_dict['sentencelength']))
-        self.metrics[self._mode]["recall"].update_state(tf.cast(tf.raw_ops.NotEqual(x=targets['tgt'],y=self._tag_string_mapper.get_oov_id()),tf.int32), true_positiv_indexes, tf.sequence_mask(graph_out_dict['sentencelength']))
+        self.metrics[self._mode]["precision"].update_state(true_positiv_indexes, tf.cast(tf.raw_ops.NotEqual(x=graph_out_dict['pred_ids'],y=self._tag_string_mapper.get_oov_id()),tf.int32), weights)
+        self.metrics[self._mode]["recall"].update_state(tf.cast(tf.raw_ops.NotEqual(x=targets['tgt'],y=self._tag_string_mapper.get_oov_id()),tf.int32), true_positiv_indexes, weights)
 
-
-if __name__ == "__main__":
-    string_mapper=get_sm('../../../data/tags/ner_uja.txt')
-    pos_indices = []
-    for id in range(string_mapper.size()):
-        tag = string_mapper.get_value(id)
-        print(tag)
-        if tag.strip() != 'UNK':
-            pos_indices.append(id)
-    print(pos_indices,string_mapper.get_channel('O'))
-
-    print(tf.cast(tf.cast([0,1,2,3],tf.bool),tf.int32).numpy())
-
-    print(tf.cast(tf.raw_ops.Equal(x=[0,1,2,3],y=[0,0,2,3]),tf.int32).numpy())
-    print(tf.cast(tf.raw_ops.NotEqual(x=[0,1,2,6],y=string_mapper.get_oov_id()),tf.int32).numpy())
-
-    m=tf.keras.metrics.Precision()
-    m.reset_states()
-    _=m.update_state([0,1,1,1,0],[1,0,1,1,1])
-    print(m.result().numpy())
 

model_fn/model_fn_nlp/util_nlp/graphs_ner.py
@@ -33,8 +33,6 @@ class NERwithMiniBERT(GraphBase):
         self._tracked_layers["softmax"] = tf.keras.layers.Softmax()
 
     def call(self, inputs, training=None, mask=None):
-        sentencelength = inputs["sentencelength"]
-        sentencelength = sentencelength[:, 0]
         inp={}
         inp["text"]=inputs["sentence"]
         bert_graph_out=self._pretrained_bert(inp)
@@ -42,6 +40,5 @@ class NERwithMiniBERT(GraphBase):
         final_output = self._tracked_layers["last_layer"](bert_graph_out["enc_output"])  # (batch_size, tar_seq_len, target_vocab_size)
         pred_ids = tf.argmax(input=final_output, axis=2, output_type=tf.int32)
         probabilities = self._tracked_layers["softmax"](final_output)
-        self._graph_out = {"pred_ids": pred_ids, 'probabilities': probabilities, 'logits': final_output,
-                           "sentencelength": sentencelength}
+        self._graph_out = {"pred_ids": pred_ids, 'probabilities': probabilities, 'logits': final_output}
         return self._graph_out