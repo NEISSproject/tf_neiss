3ec4ad38648b0377c49b8e40592f91d3aa8431f3
Konrad Sperfeld <konrad.sperfeld@uni-rostock.de>
2020-10-02 10:16:20 +0200

    Added Multisentence SOP Train mode for BERT

--- input_fn/input_fn_nlp/input_fn_bert_sop.py
+++ input_fn/input_fn_nlp/input_fn_bert_sop.py
@@ -42,6 +42,77 @@ class InputFnBertSOP(InputFnNLPBase):
         self._types = input_types, tgt_types
         self._defaults = input_defaults, tgt_defaults
 
+    def build_two_sentence_segments(self,sentences):
+        lensentences=len(sentences)
+        splitindex=random.randint(0,lensentences-2)
+        first_sentences=sentences[splitindex]
+        first_enc_sentence=self._tokenizer_de.encode(first_sentences)
+        second_sentences=sentences[splitindex+1]
+        second_enc_sentence=self._tokenizer_de.encode(second_sentences)
+        firstaddindex=splitindex-1
+        secondaddindex=splitindex+2
+        #Check if it is already to long
+        if len(first_enc_sentence)+len(second_enc_sentence)>self._max_token_text_part:
+            half=int(self._max_token_text_part/2)
+            if len(first_enc_sentence)>half:
+                first_enc_sentence=first_enc_sentence[len(first_enc_sentence)-half:]
+            if len(second_enc_sentence)>half:
+                second_enc_sentence=second_enc_sentence[:half]
+        else:
+            #Attempt to extend
+            stop=False
+            while not stop:
+                if firstaddindex<0 and secondaddindex>=lensentences:
+                    stop=True
+                elif firstaddindex<0:
+                    stopback=False
+                    while not stopback:
+                        new_sentences=second_sentences+' '+sentences[secondaddindex]
+                        new_enc_sentence=self._tokenizer_de.encode(new_sentences)
+                        if len(first_enc_sentence)+len(new_enc_sentence)<=self._max_token_text_part:
+                            second_sentences=new_sentences
+                            second_enc_sentence=new_enc_sentence
+                            secondaddindex+=1
+                            if secondaddindex>=lensentences:
+                                stopback=True
+                        else:
+                            stopback=True
+                    stop=True
+                elif secondaddindex>=lensentences:
+                    stopfront=False
+                    while not stopfront:
+                        new_sentences=sentences[firstaddindex]+' '+first_sentences
+                        new_enc_sentence=self._tokenizer_de.encode(new_sentences)
+                        if len(second_enc_sentence)+len(new_enc_sentence)<=self._max_token_text_part:
+                            first_sentences=new_sentences
+                            first_enc_sentence=new_enc_sentence
+                            firstaddindex-=1
+                            if firstaddindex<0:
+                                stopfront=True
+                        else:
+                            stopfront=True
+                    stop=True
+                else:
+                    if random.choice([True, False]):
+                        new_sentences=sentences[firstaddindex]+' '+first_sentences
+                        new_enc_sentence=self._tokenizer_de.encode(new_sentences)
+                        if len(first_enc_sentence)+len(second_enc_sentence)+len(new_enc_sentence)<=self._max_token_text_part:
+                            first_sentences=new_sentences
+                            first_enc_sentence=new_enc_sentence
+                            firstaddindex-=1
+                        else:
+                            firstaddindex=-1
+                    else:
+                        new_sentences=second_sentences+' '+sentences[secondaddindex]
+                        new_enc_sentence=self._tokenizer_de.encode(new_sentences)
+                        if len(first_enc_sentence)+len(second_enc_sentence)+len(new_enc_sentence)<=self._max_token_text_part:
+                            second_sentences=new_sentences
+                            second_enc_sentence=new_enc_sentence
+                            secondaddindex+=1
+                        else:
+                            secondaddindex=lensentences
+        return first_enc_sentence,second_enc_sentence
+
     def _parse_fn(self, sentences):
         if self._flags.segment_train:
             inputlist=sentences.split(' ')
@@ -60,8 +131,7 @@ class InputFnBertSOP(InputFnNLPBase):
             if len(sec_enc_sentence)>self._max_token_text_part:
                 sec_enc_sentence=sec_enc_sentence[:self._max_token_text_part]
         else:
-            first_enc_sentence=self._tokenizer_de.encode(sentences[0])
-            sec_enc_sentence=self._tokenizer_de.encode(sentences[1])
+            first_enc_sentence, sec_enc_sentence=self.build_two_sentence_segments(sentences)
         first_mask_enc_sentence,first_masked_index_list=self.mask_enc_sentence(first_enc_sentence)
         sec_mask_enc_sentence,sec_masked_index_list=self.mask_enc_sentence(sec_enc_sentence)
         #Add CLS-Tag and SEP-Tag
@@ -82,7 +152,10 @@ class InputFnBertSOP(InputFnNLPBase):
 
     def generator_fn(self):
         for element in self._worklist:
-            if len(element.split(' '))>10:
+            if self._flags.segment_train:
+                if len(element.split(' '))>10:
+                    yield self._parse_fn(element)
+            else:
                 yield self._parse_fn(element)
 
     def generator_fn_predict(self):
